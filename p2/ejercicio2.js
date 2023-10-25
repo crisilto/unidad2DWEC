6017d3a50fa41fd54801e503e540da6bd345bc5a
crisilto <cristinasilvestre1708@gmail.com>
2023-10-25 08:32:44 +0200

    Update ejercicio2.js

--- p2/ejercicio2.js
+++ p2/ejercicio2.js
@@ -12,4 +12,12 @@ let diasSemana = [
 document.getElementById("diasSemana").innerHTML = `Días de la semana: ${diasSemana}`;
 
 //O lo podemos mostrar en la consola del navegador a través del documento HTML
-console.log(`Días de la semana: ${diasSemana}`);
+//Con foreach
+diasSemana.forEach(dia => {
+    console.log(dia);
+});
+
+//Con for
+for (let i = 0; i < diasSemana.length; i++) {
+    console.log(diasSemana[i]);
+}
